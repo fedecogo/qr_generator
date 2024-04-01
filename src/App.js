9d220c8edbfdc6bbf100cd17fc009e8f56223f7a
fedecogo <141748020+fedecogo@users.noreply.github.com>
2024-04-01 05:56:44 +0200

    ...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import MainComponent from "./src/components/MainComponent"
+import MainComponent from ".src/components/MainComponent.jsx"; 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <MainComponent/>
-   
     </div>
   );
 }
